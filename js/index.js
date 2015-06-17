3f2aa06d7d29e603b9c48e76306376b2780de956
asdmay <wrbmc.mc.pr@gmail.com>
2015-06-18 01:00:17 +0900

    jsのテキスト3章まで終わった

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-document.write("初めてjsから文章を出力しているよ");
-document.bgColor = "yellow";
-document.fgColor = "blue";
+// document.write("初めてjsから文章を出力しているよ");
+// document.bgColor = "yellow";
+// document.fgColor = "blue";
+var message;
+message = "こんにちわ";
+
+
+var age;
+age = 21;
+document.write(message+"<br>");
+document.write("<h1>私の年齢は"+age+"です</h1>");
+// document.bgColor = "yellow";
+// document.fgColor = "blue";
+
+var height;
+height = prompt("身長を入力してください",160);
+document.write("私の身長は"+height+"です");
