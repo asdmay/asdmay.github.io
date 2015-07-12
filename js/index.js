9978f6ccb7065fed92fb5fd3f1d410237a396dd2
asdmay <wrbmc.mc.pr@gmail.com>
2015-07-12 14:39:07 +0900

    6章終わった！

--- js/index.js
+++ js/index.js
@@ -1,121 +1,159 @@
 // document.write("初めてjsから文章を出力しているよ");
 // document.bgColor = "yellow";
 // document.fgColor = "blue";
-var message;
-message = "こんにちわ";
+// var message;
+// message = "こんにちわ";
 
 
-var age;
-age = 21;
-document.write(message+"<br>");
-document.write("<h1>私の年齢は"+age+"です</h1>");
+// var age;
+// age = 21;
+// document.write(message+"<br>");
+// document.write("<h1>私の年齢は"+age+"です</h1>");
 // document.bgColor = "yellow";
 // document.fgColor = "blue";
 
-var height;
-var refheight = 160;
-height = prompt("身長を入力してください",160);
-document.write("あなたの身長が"+height+"ってことは<br>");
+// var height;
+// var refheight = 160;
+// height = prompt("身長を入力してください",160);
+// document.write("あなたの身長が"+height+"ってことは<br>");
 
-if (height >= refheight){
-    document.write("私より背が高いですね");
-}else{
-    document.write("ふーん");
-}
+// if (height >= refheight){
+//     document.write("私より背が高いですね");
+// }else{
+//     document.write("ふーん");
+// }
 
-var month;
-month = prompt("今月は何月ですか？",1);
-month = Number(month);
+// var month;
+// month = prompt("今月は何月ですか？",1);
+// month = Number(month);
 
-switch(month){
-case 1:
-    document.write("睦月");
-    break;
-case 2:
-    document.write("如月");
-    break;
-case 3:
-    document.write("弥生");
-    break;
-case 4:
-    document.write("卯月");
-    break;
-case 5:
-    document.write("皐月");
-    break;
-case 6:
-    document.write("水無月");
-    break;
-case 7:
-    document.write("文月");
-    break;
-case 8:
-    document.write("葉月");
-    break;
-case 9:
-    document.write("長月");
-    break;
-case 10:
-    document.write("神無月");
-    break;
-case 11:
-    document.write("霜月");
-    break;
-case 12:
-    document.write("師走");
-    break;
-default:
-    document.write("ちゃんとして！");
-}
+// switch(month){
+// case 1:
+//     document.write("睦月");
+//     break;
+// case 2:
+//     document.write("如月");
+//     break;
+// case 3:
+//     document.write("弥生");
+//     break;
+// case 4:
+//     document.write("卯月");
+//     break;
+// case 5:
+//     document.write("皐月");
+//     break;
+// case 6:
+//     document.write("水無月");
+//     break;
+// case 7:
+//     document.write("文月");
+//     break;
+// case 8:
+//     document.write("葉月");
+//     break;
+// case 9:
+//     document.write("長月");
+//     break;
+// case 10:
+//     document.write("神無月");
+//     break;
+// case 11:
+//     document.write("霜月");
+//     break;
+// case 12:
+//     document.write("師走");
+//     break;
+// default:
+//     document.write("ちゃんとして！");
+// }
 
-var MAX = 8;
-var i = 1;
-while(i <= MAX){
-    document.write(i);
-    document.write("西暦", 1988 + i ,"年<br>");
-    i++;
-}
+// var MAX = 8;
+// var i = 1;
+// while(i <= MAX){
+//     document.write(i);
+//     document.write("西暦", 1988 + i ,"年<br>");
+//     i++;
+// }
 
-var ans = 17;
-var num;
-var msg = "好きな数字を入力して下さい";
+// var ans = 17;
+// var num;
+// var msg = "好きな数字を入力して下さい";
 
-while(true){
-    num = prompt(msg, 0);
-    if(ans == num){
-	document.write("正解");
-	break;
-    }
+// while(true){
+//     num = prompt(msg, 0);
+//     if(ans == num){
+// 	document.write("正解");
+// 	break;
+//     }
     
-    if(ans > num){
-	msg = "もっと大きい数字に変えて";
-    }else if(ans < num){
-	msg = "もっと小さい数字に変えて";
-    }else{
-	document.write("数字を入力して下さい");
-    }
-}
+//     if(ans > num){
+// 	msg = "もっと大きい数字に変えて";
+//     }else if(ans < num){
+// 	msg = "もっと小さい数字に変えて";
+//     }else{
+// 	document.write("数字を入力して下さい");
+//     }
+// }
 
-function dollToyen(doll, rate){
-    var yen;
-    yen = doll * rate;
-    return yen;
-}
+// function dollToyen(doll, rate){
+//     var yen;
+//     yen = doll * rate;
+//     return yen;
+// }
 
-var yen1 = dollToyen(100, 90);
-document.write(yen1+ "円");
+// var yen1 = dollToyen(100, 90);
+// document.write(yen1+ "円");
 
-var doll2 = 500;
-var rate2 = 80;
+// var doll2 = 500;
+// var rate2 = 80;
 
-var yen2 = dollToyen(doll2, rate2);
-document.write(yen2+"円");
+// var yen2 = dollToyen(doll2, rate2);
+// document.write(yen2+"円");
 
-function testFunc(){
+// function testFunc(){
    // var num;
-    num = 5;
+    // num = 5;
+//}
+
+// var num = 3;
+// testFunc();
+// document.write(num);
+
+// var now;
+// now = new Date();
+// document.write(now.toString());
+
+// var aDay;
+// aDay = new Date(1993, 11, 9, 12, 12, 0);
+// document.write(aDay.toString(),"<br>");
+
+// var  diff, days;
+// document.write(now.getFullYear() +"年"
+// 	       + (now.getMonth()+1) +"月"
+// 	       + now.getDate() +"日");
+// diff = now.getTime() - aDay.getTime();
+// days = Math.ceil(diff/(24 * 60 * 60 * 1000));
+// document.write(days);
+
+var num = Math.max(78, 92, 14, 27, 88);
+document.write(Math.max(78, 92, 14, 27, 88));
+document.write(Math.min(78, 92, 14, 27, 88));
+
+var random = Math.random();
+if(random < 0.33){
+    document.write("大吉");
+}else if(random < 0.66){
+    document.write("吉");
+}else{
+    document.write("凶");
 }
 
-var num = 3;
-testFunc();
-document.write(num);
+var str = new String("たのしいJavaScript");
+document.write(str,str.length);
+for(var i = 0; i <= (str.length - 1); i++){
+    document.write(str.charAt(i),"<br>");
+}
+var url = "http://www.yahoo.co.jp"
+var text ="yahoo".link(url) + "で検索";
+document.write(text);
