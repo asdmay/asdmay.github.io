ae8fbea285144ce007a0d28086f7fc34287e29a1
asdmay <wrbmc.mc.pr@gmail.com>
2019-01-02 03:19:31 +0900

    WorksとTitleを作った

--- portfolio/src/Works.js
+++ portfolio/src/Works.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Title from './Title';
 
-const Works = (
-  <section>
-    <h1>Works</h1>
-  </section>
-);
+const works = [
+  {
+    title: 'hoge',
+    description: 'もにゃもにゃ',
+    img: 'sample.png'
+  },
+  {
+    title: 'fuga',
+    description: 'もにゃもにゃ',
+    img: 'sample.png'
+  }
+];
+
+class Works extends Component {
+  render() {
+    return (
+      <section>
+        <Title title="Works" />
+        <ul>
+            <li>
+              <h2>{works[0].title}</h2>
+              <p>{works[0].description}</p>
+            </li>
+            <li>
+              <h2>{works[1].title}</h2>
+              <p>{works[1].description}</p>
+            </li>
+          }
+        </ul>
+      </section>
+    );
+  }
+}
 
 export default Works;
