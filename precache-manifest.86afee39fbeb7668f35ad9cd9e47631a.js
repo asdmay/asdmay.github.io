601faf2f09c5ba340ffe3736f1926f99dabb8f7c
asdmay <wrbmc.mc.pr@gmail.com>
2019-01-08 00:45:08 +0900

    Updates

---
+++ precache-manifest.86afee39fbeb7668f35ad9cd9e47631a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "64cf8e78a22fc163cd24",
+    "url": "/portfolio/static/js/main.64cf8e78.chunk.js"
+  },
+  {
+    "revision": "bacb2127844695338147",
+    "url": "/portfolio/static/js/1.bacb2127.chunk.js"
+  },
+  {
+    "revision": "64cf8e78a22fc163cd24",
+    "url": "/portfolio/static/css/main.b43a3056.chunk.css"
+  },
+  {
+    "revision": "bacb2127844695338147",
+    "url": "/portfolio/static/css/1.da1c16bb.chunk.css"
+  },
+  {
+    "revision": "76462c35d9795680ae2b24b13d2ff919",
+    "url": "/portfolio/index.html"
+  }
+];
