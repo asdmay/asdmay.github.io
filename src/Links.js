43e690cc4af93cd78ab750188e7df18f83cebbc6
asdmay <wrbmc.mc.pr@gmail.com>
2019-01-15 01:30:17 +0900

    listのコンポーネントを作ってみる

--- src/Links.js
+++ src/Links.js
@@ -1,46 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Title from './Title';
 
-class Links extends Component {
-  render(){
-    return (
-      <section>
-        <Title title="Links" />
-        <ul>
-          <li>
-            <a
-              className="Links-link"
-              href="https://www.facebook.com/?locale=ja_JP"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              FaceBook
-            </a>
-          </li>
-          <li>
-            <a
-              className="Links-link"
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </a>
-          </li>
-          <li>
-            <a
-              className="Links-link"
-              href="https://www.instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             Instagram 
-            </a>
-          </li>
-        </ul>
-      </section>
-    );
-  }
+function LinkList(props) {
+  const links = props.links;
+  const listItems = links.map((links) => 
+    <li className="LiskList-item" key={links.key}><a target="_blank" rel="noopener noreferrer" href={links.url}>{(links.title)+"だよ"}</a></li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
 }
+
+const links = [
+  {id: 1, title: "Facebook", url: "https://www.facebook.com/?locale=ja_JP"},
+  {id: 2, title: "Twitter", url: "https://twitter.com/"} ,
+  {id: 3, title: "Instagram", url: "https://www.instagram.com"}
+];
+
+function Links(props){
+  return (
+    <section>
+      <Title title="Links" />
+      <LinkList links={links} /> 
+    </section>
+  )
+};
 
 export default Links;
