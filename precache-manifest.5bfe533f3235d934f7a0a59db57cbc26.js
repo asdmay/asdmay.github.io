188b0eae20869edc62d4451877a08928f604be29
Maaya Ashida <maaya@MaayanoMacBook-Pro.local>
2019-08-25 15:20:46 +0900

    Updates

--- precache-manifest.5bfe533f3235d934f7a0a59db57cbc26.js
+++ precache-manifest.5bfe533f3235d934f7a0a59db57cbc26.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac28e6a11e775fa88ce92c3e10618fb1",
+    "revision": "d6fd2d3fa4c1c921ab3fc95e77f83cc7",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.05be81b1.chunk.css"
   },
   {
-    "revision": "96813b5dc9b3f3151b3e",
-    "url": "/static/css/main.795991f2.chunk.css"
+    "revision": "0fe0bb69157aeb50e0a9",
+    "url": "/static/css/main.bd54819e.chunk.css"
   },
   {
     "revision": "3b743fad2dbe51d2db69",
     "url": "/static/js/2.a17c2e7c.chunk.js"
   },
   {
-    "revision": "96813b5dc9b3f3151b3e",
-    "url": "/static/js/main.393d4a71.chunk.js"
+    "revision": "0fe0bb69157aeb50e0a9",
+    "url": "/static/js/main.2a240b3a.chunk.js"
   },
   {
     "revision": "c49018507a564c0ada45",
