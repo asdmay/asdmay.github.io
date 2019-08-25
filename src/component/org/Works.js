5d1a6cffbab4f19f64631aa1e01eac3d8f22a04f
Maaya Ashida <maaya@MaayanoMacBook-Pro.local>
2019-08-25 21:11:07 +0900

    fix img path

--- src/component/org/Works.js
+++ src/component/org/Works.js
@@ -6,21 +6,21 @@ const works = [
   {
     title: 'おもちぐまのスタンプ',
     text: 'オリジナルキャラのおもちぐまのスタンプ。まじかわいい。便利。',
-    src: '../../img/omochiguma.png',
+    src: '/img/omochiguma.png',
     art: 'おもちぐまのスタンプ',
     href: 'https://store.line.me/stickershop/product/7154677/ja'
   },
   {
     title: 'KUMA NO STAMP ~1st IMPACT~',
     text: 'ゆるく使えるLINEスタンプ',
-    src: '../../img/kuma_stamp.png',
+    src: './img/kuma_stamp.png',
     art: 'KUMA NO STAMP',
     href: 'https://store.line.me/stickershop/product/1333908/ja?from=sticker'
   },
   {
     title: 'hoge',
     text: 'ゆるく使えるLINEスタンプ',
-    src: '../../img/vui.png',
+    src: './img/vui.png',
     art: 'hoge',
     href: 'https://store.line.me/stickershop/product/1333908/ja?from=sticker'
   }
