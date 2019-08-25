0b2e62d4f0d799d7823bffadf6c910961cd4e6b0
Maaya Ashida <maaya@MaayanoMacBook-Pro.local>
2019-08-25 20:41:45 +0900

    imgの場所を変更

--- src/component/org/Works.js
+++ src/component/org/Works.js
@@ -6,22 +6,22 @@ const works = [
   {
     title: 'おもちぐまのスタンプ',
     text: 'オリジナルキャラのおもちぐまのスタンプ。まじかわいい。便利。',
-    src: './img/omochiguma.png',
+    src: '../../img/omochiguma.png',
     art: 'おもちぐまのスタンプ',
     href: 'https://store.line.me/stickershop/product/7154677/ja'
   },
   {
     title: 'KUMA NO STAMP ~1st IMPACT~',
     text: 'ゆるく使えるLINEスタンプ',
-    src: './img/kuma_stamp.png',
+    src: '../../img/kuma_stamp.png',
     art: 'KUMA NO STAMP',
     href: 'https://store.line.me/stickershop/product/1333908/ja?from=sticker'
   },
   {
-    title: '',
+    title: 'hoge',
     text: 'ゆるく使えるLINEスタンプ',
-    src: './img/vui.png',
-    art: '',
+    src: '../../img/vui.png',
+    art: 'hoge',
     href: 'https://store.line.me/stickershop/product/1333908/ja?from=sticker'
   }
 ];
@@ -35,7 +35,7 @@ function Works(props) {
       <SectionTitle>Works</SectionTitle>
       <ul>
         {
-          works.map((works, index) => 
+          works.map((works, index) =>
           <Work key={toString(index)}{...works}/>
           )
         }
