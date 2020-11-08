35c211165133b8e320ea2a14b4ce63b90ad59a93
Maaya Ashida <maaya@MaayanoMacBook-Pro.local>
2020-11-08 21:45:04 +0900

    AboutMe一旦完成

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -9,7 +9,7 @@ const Section = styled.section`
 `
 
 const Title = styled.h1`
-  font-size: 4.0rem;
+  font-size: 2.5rem;
   color: rgba(0,0,0,0.9);
 `
 
@@ -17,12 +17,15 @@ const Image = styled.img`
 `
 
 const TextArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center; 
   max-width: 50%;
-  padding-left: 16px;
+  padding-left: 32px;
 `
 
 const Text = styled.p`
-  font-size: 1.6rem;
+  font-size: 1.3rem;
   color: rgba(0,0,0,0.9);
 `
 
@@ -37,9 +40,9 @@ const AboutMe = () =>{
       <TextArea>
       <Title>MAAYA ASHIDA</Title>
       <BoldText>“ I like to solve problems by designing. “</BoldText>
-      <Text>I’m MAAYA.
-    When I was a student, I majored in User Interaction and Information Compilation.
-    After that, I joined Yahoo! JAPAN and became a UX designer.
+      <Text>I’m MAAYA.<br></br>
+    When I was a student, I majored in User Interaction and Information Compilation.<br></br>
+    After that, I joined Yahoo! JAPAN and became a UX designer.<br></br>
     I design UI/ UX to make the future better.</Text>
       </TextArea>
     </Section>
